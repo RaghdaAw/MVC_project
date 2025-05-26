e5f93144d0439e2e677bdf6916426b56ed351bfd
Raghda <9015808@student.zadkine.nl>
2025-05-26 20:59:52 +0200

    bookconnect

---
+++ controller/BookConnect.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'models/Book.php';
+
+class BookController
+{
+    private $bookModel;
+
+    public function __construct($pdo)
+    {
+        $this->bookModel = new Book($pdo);
+    }
+
+    public function showAllBooks()
+    {
+        $books = $this->bookModel->getAllBooks();
+        include 'view/book_list.php'; // الملف الذي يحتوي على HTML
+    }
+}
