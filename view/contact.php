9c4ecab9dbdce907d667f4e39ede8f3dc77762ed
Ni-Yara <9021288@student.zadkine.nl>
2025-06-26 20:12:00 +0200

    cart and contact page CSS

--- view/contact.php
+++ view/contact.php
@@ -7,14 +7,10 @@
 
 </head>
 <body>
-    <?php 
-       include 'navbar.php';
 
-
-    ?>
-    <div class="contact-container">
-        <h2>Contact Us</h2>
-        <form action="contact.php" method="post">
+<!--        <a class="back-to-home-button" href="/public.php?page=userDashboard"> Back to Home </a>-->
+        <h2 class="contact-us-text">Contact Us</h2>
+        <form class="contact-form" action="contact.php" method="post">
             <input type="text" name="name" placeholder="Your Name" required>
 
             <input type="email" name="email" placeholder="Your Email" required>
@@ -31,6 +27,6 @@
             echo "<p class='success-msg'>✅ Thank you, your message has been sent.</p>";
         }
         ?>
-    </div>
+
 </body>
 </html>
